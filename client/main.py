995556089ad3fe2301b38864d6dbe642748ba6b8
Daniil Protasenia <danikprotaseney@gmail.com>
2023-12-11 05:04:37 +0300

    refactor(structure): remove structure

--- client/main.py
+++
@@ -1,7 +0,0 @@
-from worker_manager import run_workers
-from Multiprocessing_task.constant import LOCAL_HOST
-
-if __name__ == "__main__":
-    master_address = (LOCAL_HOST, 9091)
-    num_workers = 5
-    run_workers(master_address, num_workers)
--- server/main.py
+++
@@ -1,5 +0,0 @@
-from master import master
-
-if __name__ == "__main__":
-    master_port = 9091
-    master(master_port)

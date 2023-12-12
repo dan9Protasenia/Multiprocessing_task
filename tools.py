8fccd8f34aca74880e2134716923e54136df3e02
Daniil Protasenia <danikprotaseney@gmail.com>
2023-12-12 20:49:26 +0300

    refactor(tools): redit logic of the log_metrics function and moved the logger

--- tools.py
+++ tools.py
@@ -3,19 +3,25 @@ import time
 import logging
 
 
-def write_metrics_to_file(self, metric_data):
-    with open(self.output_file, mode='a') as file:
-        file.write(json.dumps(metric_data) + '\n')
+def setup_logger():
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
 
 
-def log_metrics(self, count_type):
+def log_metrics(count_type, metric_data, output_file):
     current_time = int(time.time())
-    metric_data = {
-        "timestamp": current_time,
-        "count_type": count_type,
-        "A1_sum": self.metric_data_10s["A1_sum"],
-        "A2_max": self.metric_data_10s["A2_max"],
-        "A3_min": self.metric_data_10s["A3_min"]
-    }
+    metric_data["timestamp"] = current_time
+    metric_data["count_type"] = count_type
+
     logging.info(f"Metrics for {count_type}: {metric_data}")
-    write_metrics_to_file(metric_data)
+
+    with open(output_file, mode='a') as file:
+        file.write(json.dumps({
+            "timestamp": metric_data["timestamp"],
+            "count_type": metric_data["count_type"],
+            "A1_sum": metric_data["A1_sum"],
+            "A2_max": metric_data["A2_max"],
+            "A3_min": metric_data["A3_min"]
+        }) + '\n')
